451aebc81e820afe2c0c13b71d0f073aae833dc6
Gastón Díaz <gipdiaz@gmail.com>
2015-05-12 08:29:34 -0300

    algo

--- app/models/altas_bajas_hora.rb
+++ app/models/altas_bajas_hora.rb
@@ -1,4 +1,9 @@
 class AltasBajasHora < ActiveRecord::Base
   belongs_to :establecimiento
   belongs_to :persona
+
 end
+
+#SELECT * FROM detalle d inner join recibos r on r.nume_docu = d.nume_docu where d.nume_docu = 30284359 and d.mes = 4 and d.anio = 2015 LIMIT 0,1000
+#SELECT * FROM detalle d where d.nume_docu = 30284359 and d.mes = 4 and d.anio = 2015 LIMIT 0,1000
+# TRAER CARGOS ACTIVOS DE UNA PERSONA SELECT * FROM paddoc p where nume_docu = 30284359 and estado = "ALT" LIMIT 0,1000
