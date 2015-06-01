e44c45c236c73a6080a36c706d8a39588ad36ba5
Marcos Litterio <marcos.litterio@gmail.com>
2015-06-01 16:10:16 -0300

    botones en baja

--- app/datatables/altas_bajas_hora_baja_efectiva_datatable.rb
+++ app/datatables/altas_bajas_hora_baja_efectiva_datatable.rb
@@ -22,6 +22,10 @@ class AltasBajasHoraBajaEfectivaDatatable < AjaxDatatablesRails::Base
         record.persona.cuil,
         '<span class="label label-info">'+Util.fecha_a_es(record.fecha_alta)+'</span>',
         '<span class="label label-success">'+Util.fecha_a_es(record.fecha_baja)+'</span>',
+        '<center><div class="btn-acciones">'+ 
+          '<a class="btn btn-sm btn-danger" data-toggle="tooltip" data-placement="top" title="Cancelar baja" href="'+Rails.application.routes.url_helpers.altas_bajas_horas_cancelar_baja_path(record.id.to_s)+'"><span class="glyphicon glyphicon-remove" aria-hidden="true" ></span></a>'+
+          '<a class="btn btn-success btn-sm" data-toggle="tooltip" data-placement="top" title="Marcar como chequeada" href="'+Rails.application.routes.url_helpers.altas_bajas_horas_chequear_baja_path(record.id.to_s)+'"><span class="glyphicon glyphicon-ok" aria-hidden="true" ></span></a>'+
+        '</div></center>', 
       ]
     end
   end
