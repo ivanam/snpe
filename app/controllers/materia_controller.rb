6024b2263068ff781370db7e94ea4cb9ddc0d1bc
martin <ttincho77gmail.com>
2016-08-24 10:47:38 -0300

    Agrego datatables de materia y plan

--- app/controllers/materia_controller.rb
+++ app/controllers/materia_controller.rb
@@ -1,6 +1,47 @@
 class MateriaController < InheritedResources::Base
+  before_action :set_materium, only: [:show, :edit, :update, :destroy]
+
+  respond_to :html
+
+  def index
+    respond_to do |format|
+      format.html
+      format.json { render json: MateriaDatatable.new(view_context, { query: Materium.all }) }
+    end
+  end
+
+  def show
+    respond_with(@materium)
+  end
+
+  def new
+    @materium = Materium.new
+    respond_with(@materium)
+  end
+
+  def edit
+  end
+
+  def create
+    @materium = Materium.new(materium_params)
+    @materium.save
+    respond_with(@materium)
+  end
+
+  def update
+    @materium.update(materium_params)
+    respond_with(@materium)
+  end
+
+  def destroy
+    @materium.destroy
+    respond_with(@materium)
+  end
 
   private
+    def set_materium
+      @materium = Materium.find(params[:id])
+    end
 
     def materium_params
       params.require(:materium).permit(:codigo, :descripcion)
---
+++ app/datatables/materia_datatable.rb
@@ -0,0 +1,44 @@
+class MateriaDatatable < AjaxDatatablesRails::Base
+  include AjaxDatatablesRails::Extensions::WillPaginate
+
+  def sortable_columns
+    # Declare strings in this format: ModelName.column_name
+    @sortable_columns ||= ['materia.id']
+  end
+
+  def searchable_columns
+    # Declare strings in this format: ModelName.column_name
+    @searchable_columns ||= ['materia.id']
+  end
+
+  private
+
+  def data
+    records.map do |record|
+      [
+        # comma separated list of the values for each cell of a table row
+        # example: record.attribute,
+        record.codigo,
+        record.descripcion,
+        '<div class="dropdown">
+          <button class="btn btn-primary dropdown-toggle" type="button" id="dropdownMenu1" data-toggle="dropdown">
+            Acciones
+            <span class="caret"></span>
+          </button>
+          <ul class="dropdown-menu" role="menu" aria-labelledby="dropdownMenu1">
+            <li role="presentation"><a role="menuitem" tabindex="-1" href="materia/'+record.id.to_s+'">Ver</a></li>
+            <li role="presentation"><a role="menuitem" tabindex="-1" href="materia/'+record.id.to_s+'/edit">Editar</a></li>
+            <li role="presentation"><a rel="nofollow" data-method="delete" data-confirm="Seguro desea eliminar" role="menuitem" tabindex="-1" href="materia/'+record.id.to_s+'">Eliminar</a></li>
+          </ul>
+        </div>',
+      ]
+    end
+  end
+
+  def get_raw_records
+    # insert query here
+    return options[:query]
+  end
+
+  # ==== Insert 'presenter'-like methods below if necessary
+end
