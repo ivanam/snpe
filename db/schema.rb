278bea3739c67554365f5ef59965faef40e45bd8
martin <ttincho77gmail.com>
2016-10-18 11:32:42 -0300

    Agrego scaffold de Turno

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20161006144729) do
+ActiveRecord::Schema.define(version: 20161018135616) do
 
   create_table "active_admin_comments", force: true do |t|
     t.string   "namespace"
@@ -501,6 +501,12 @@ ActiveRecord::Schema.define(version: 20161006144729) do
 
   create_table "tipo_documentos", force: true do |t|
     t.string   "nombre"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
+
+  create_table "turnos", force: true do |t|
+    t.string   "descripcion"
     t.datetime "created_at"
     t.datetime "updated_at"
   end
