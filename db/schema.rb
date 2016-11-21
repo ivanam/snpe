bbf82d7e10d6932fd5695d8bdaa89b572a601b2d
ivana <iva.moyano@gmail.com>
2016-11-21 11:06:00 -0300

    agregar modelo de funciones y cargosnd4

--- db/schema.rb
+++ db/schema.rb
@@ -11,11 +11,8 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-<<<<<<< HEAD
-ActiveRecord::Schema.define(version: 20161121134945) do
-=======
+
 ActiveRecord::Schema.define(version: 20161104121214) do
->>>>>>> bd8ee9bdc21f8d32f07b936a3ccec1a1d37f6a17
 
   create_table "active_admin_comments", force: true do |t|
     t.string   "namespace"
