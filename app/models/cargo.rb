b7bd3f88c36a0f863941f19aec2b684a6f0ef412
Gastón Díaz <gipdiaz@gmail.com>
2017-03-23 13:52:14 -0300

    Se agregaron controles en cargos

--- app/models/cargo.rb
+++ app/models/cargo.rb
@@ -10,16 +10,16 @@ class Cargo < ActiveRecord::Base
 
   validate :cargo_jerarquico
 
-  validate :situacion_revista
+  validate :sit_revista
 
   #-----------------------------------------------------------------------------------------------------------
   def cargo_ocupado
     "Revisa si existe una persona en el cargo"
-    if self.establecimiento.nivel_id == 1 # Inicial
+    if self.establecimiento.nivel_id.to_i == 1 # Inicial
       cargo_ocupado_primaria(self.establecimiento_id, self.cargo)
-    elsif self.establecimiento.nivel_id == 2 # Primaria
+    elsif self.establecimiento.nivel_id.to_i == 2 # Primaria
       cargo_ocupado_primaria(self.establecimiento_id, self.cargo)
-    elsif self.establecimiento.nivel_id == 3 # Secundaria
+    elsif self.establecimiento.nivel_id.to_i == 3 # Secundaria
       print "lala"
     else
       errors.add(:establecimiento, "no tiene nivel asignado")
@@ -28,8 +28,10 @@ class Cargo < ActiveRecord::Base
 
   def cargo_ocupado_primaria(establecimiento_id, cargo)
     if (Funcion::DIRECTOR_CATEGORIAS.include? cargo) || (Funcion::VICEDIRECTOR_CATEGORIAS.include? cargo)
-      if Cargo.where(establecimiento_id: establecimiento_id, cargo: Funcion.cargos_equivalentes(cargo)) != []
-        errors.add(:persona, "no puede tomar el cargo ya se encuentra ocupado")
+      cargos = Cargo.where(establecimiento_id: establecimiento_id, cargo: Funcion.cargos_equivalentes(cargo))
+      if cargos != []
+        #self.sit_revista
+        errors.add(:base, self.persona.to_s + "no puede tomar el cargo ya se encuentra ocupado por" + cargos.first.persona.to_s)
       end
     end
   end
@@ -43,17 +45,23 @@ class Cargo < ActiveRecord::Base
     end
   end
 
-  def situacion_revista
-    "Revisa si corresponde la sitacion revista"
+  def sit_revista
+    # Revisa si corresponde la sitacion revista
     if !(Funcion::DIRECTOR_CATEGORIAS.include? self.cargo) || (Funcion::VICEDIRECTOR_CATEGORIAS.include? self.cargo)
-      debugger
-      cargo_actual = Cargo.where(establecimiento_id: self.establecimiento_id, cargo: self.cargo, turno: self.turno, estado: "ALT")
-      if  cargo_actual != []
+      # Cargos jerarquicos
+      cargo_actuales = Cargo.where(establecimiento_id: self.establecimiento_id, cargo: self.cargo, turno: self.turno, estado: "ALT")
+      if  cargo_actuales != []
+      # Existen cargos
         if self.situacion_revista == "1-1"
-          cargo_actual.first
+          errors.add(:base, self.persona.to_s + ": el cargo ya se encuentra ocupado el cargo por " + cargo_actuales.first.persona.to_s + " debe realizar la baja del cargo anterior")
+        elsif self.situacion_revista == "1-2"
+          errors.add(:persona, "ya se encuentra ocupado el cargo la baja del cargo anterior")
         end
       else
-
+      # No existen cargos
+        if (self.situacion_revista == "1-3") || (self.situacion_revista == "2-1") || (self.situacion_revista == "2-2") || (self.situacion_revista == "2-3") || (self.situacion_revista == "2-4") || (self.situacion_revista == "2-5")
+          errors.add(:base, "No posee titular o interino para reemplazar o suplir")
+        end
       end
     end
   end
