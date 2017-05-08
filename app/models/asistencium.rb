7cffc0e8e3833240703c881ff422498451baf4a1
martin <ttincho77gmail.com>
2017-05-08 09:25:54 -0300

    Agrego modulo de asistencias

--- app/models/asistencium.rb
+++ app/models/asistencium.rb
@@ -6,6 +6,12 @@ class Asistencium < ActiveRecord::Base
   #Creo que la asistencia debe tener su propio campo en sí mismo, ya que el registro de asistencias es mensual. Por lo cual esto no iria.
   has_many :periodos, :class_name => 'PeriodoAsistencia', :foreign_key => 'asistencia_id', dependent: :destroy
 
+  has_many :altas_bajas_horas, class_name: "AltasBajasHora", foreign_key: "altas_bajas_hora_id"
+
+  has_many :alta_cargos, class_name: "Cargo", foreign_key: "altas_bajas_cargo_id"
+
+  has_many :alta_cargo_no_docentes, class_name: "CargoNoDocente", foreign_key: "altas_bajas_cargo_no_docente_id"
+
   attr_default :ina_justificada, 0
   attr_default :ina_injustificada, 0
   attr_default :lleg_tarde_justificada, 0
