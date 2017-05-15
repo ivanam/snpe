feacccde500d65d308e087fa78df6e98752b2acf
pepeu <marcelo.eduardo.gonzalez@gmail.com>
2017-05-15 10:05:44 -0300

    cargos no docentes  gonzalo pritzker

---
+++ app/datatables/listado_licencia_cargos_datatable.rb
@@ -0,0 +1,42 @@
+class ListadoLicenciaCargosDatatable < AjaxDatatablesRails::Base
+  include AjaxDatatablesRails::Extensions::WillPaginate
+
+  def sortable_columns    
+    #@sortable_columns ||= ['establecimientos.codigo_jurisdiccional', 'personas.nro_documento','personas.apeynom','licencia.altas_bajas_hora_id','licencia.altas_bajas_hora_id','licencia.fecha_desde','licencia.fecha_hasta','licencia.articulo_id']
+    @sortable_columns ||= ['Licencium.fecha_desde', 'Licencium.fecha_hasta']
+  end
+
+  def searchable_columns
+    #@searchable_columns ||= ['establecimientos.codigo_jurisdiccional','personas.nro_documento','personas.apeynom','licencia.altas_bajas_hora_id','licencia.altas_bajas_hora_id','licencia.fecha_desde','licencia.fecha_hasta','licencia.articulo_id']
+    @searchable_columns ||= ['Licencium.fecha_desde', 'Licencium.fecha_hasta']
+  end
+
+  private
+
+  def data
+    records.map do |record|
+      [
+            "Cargo",
+            Funcion.where(Cargo.find(record.cargo_id).cargo.to_i).first.to_s,
+            Cargo.where(:id => record.cargo_id.to_i).first.persona.to_s,
+            record.articulo.codigo + " - " +record.articulo.descripcion[0..30].html_safe+"...",
+            Util.fecha_a_es(record.fecha_desde),
+            Util.fecha_a_es(record.fecha_hasta),
+            if record.vigente == "Vigente" 
+              '<center>
+                <div class="btn-acciones">
+                <a class="btn btn-success btn-sm" data-toggle="modal" fecha_desde="'+Util.fecha_a_es(record.fecha_desde)+'" fecha_hasta="'+Util.fecha_a_es(record.fecha_hasta)+'" id_lic="'+record.id.to_s+'" id_art="'+record.articulo_id.to_s+'" data-target="#modal_licencia_final" title="Editar" >
+                  <span class=aria-hidden="true" >Vigente</span>
+                </a>'  
+            else
+              '<center><div class="btn-acciones"><a class="btn btn-danger btn-sm">'+record.vigente+'</a></center></div>' 
+            end,
+     ]
+    end
+  end
+
+  def get_raw_records
+    return options[:query]
+  end
+
+end
