89a61338387541f7024e04e962c7ff1d175b5f42
ivana <iva.moyano@gmail.com>
2017-05-16 09:56:52 -0300

    agrego cambio

--- app/controllers/migracion_controller.rb
+++ app/controllers/migracion_controller.rb
@@ -1,22 +1,23 @@
 class MigracionController < ApplicationController
   before_filter :authenticate_user!
-  load_and_authorize_resource
+  authorize_resource :class => false
+
+
+
 
 	def migrar_hs
 	    client = Mysql2::Client.new(:host => "172.16.0.19", :username => "guest", :password => "guest", :database => "mec")
-	      res= client.query("select secuencia as secMax, p.* from padhc p where escuela = '"+params[:esc]+"'  and estado= 'ALT' group by  nume_docu, materia, horas_cate union
+	      res= client.query("select secuencia, p.* from padhc p where escuela = '"+params[:esc]+"'  and estado= 'ALT' group by  nume_docu, materia, horas_cate union
 select MAX(secuencia) as secMax, p.* from padhc p where escuela = '"+params[:esc]+"'  and estado= 'LIC'  group by  nume_docu, materia, horas_cate")
 
 	      esc_id= Establecimiento.where(:codigo_jurisdiccional => params[:esc]).first.id
 
 	     for r in res
 
-
 			if Materium.where(codigo: r['materia']).first != nil then
 				materia_id = Materium.where(codigo: r['materia']).first.id
 			else 
 				materia_id= 0
-			end
 
 
 			if Persona.where(:nro_documento => r['nume_docu']).first == nil then
@@ -88,7 +89,7 @@ select MAX(secuencia) as secMax, p.* from padhc p where escuela = '"+params[:esc
 
 def migrar_cargos
 	    client = Mysql2::Client.new(:host => "172.16.0.19", :username => "guest", :password => "guest", :database => "mec")
-	      res= client.query("select secuencia as secMax, p.* from paddoc p where escuela = '"+params[:esc]+"'  and estado= 'ALT' group by  nume_docu, secuencia, planta_pre, tipo_emp, cargo_r union
+	      res= client.query("select secuencia, p.* from paddoc p where escuela = '"+params[:esc]+"'  and estado= 'ALT' group by  nume_docu, secuencia, planta_pre, tipo_emp, cargo_r union
 select MAX(secuencia) as secMax, p.* from paddoc p where escuela = '"+params[:esc]+"'  and estado= 'LIC' group by  nume_docu, secuencia, planta_pre, tipo_emp, cargo_r")
 
 
@@ -154,7 +155,7 @@ select MAX(secuencia) as secMax, p.* from paddoc p where escuela = '"+params[:es
 
 def migrar_auxiliares
 	    client = Mysql2::Client.new(:host => "172.16.0.19", :username => "guest", :password => "guest", :database => "mec")
-	      res= client.query("select secuencia as secMax, p.* from padaux p where escuela = '"+params[:esc]+"'  and estado= 'ALT' group by  nume_docu, secuencia, planta_pre, tipo_emp, cargo_r union
+	      res= client.query("select secuencia, p.* from padaux p where escuela = '"+params[:esc]+"'  and estado= 'ALT' group by  nume_docu, secuencia, planta_pre, tipo_emp, cargo_r union
 select MAX(secuencia) as secMax, p.* from padaux p where escuela = '"+params[:esc]+"'  and estado= 'LIC' group by  nume_docu, secuencia, planta_pre, tipo_emp, cargo_r")
 
 
@@ -206,8 +207,7 @@ select MAX(secuencia) as secMax, p.* from padaux p where escuela = '"+params[:es
 		 respond_to do |format|
     		format.js { render nothing: true }
     	end
-	
-
+	end
 
 end
 end
