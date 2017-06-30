1f514bb317d0d4b33d565b388085f6f853aaf2b5
martin <ttincho77gmail.com>
2017-06-30 12:39:35 -0300

    Agrego fecha de baja, turno y grupo a datatable de cargos

--- app/datatables/cargos_bajas_datatable.rb
+++ app/datatables/cargos_bajas_datatable.rb
@@ -2,11 +2,11 @@ class CargosBajasDatatable < AjaxDatatablesRails::Base
   include AjaxDatatablesRails::Extensions::WillPaginate
 
   def sortable_columns
-    @sortable_columns ||= ['Persona.nro_documento', 'Persona.cuil', 'Persona.apeynom','Cargo.secuencia', 'Establecimiento.nombre','Cargo.anio','Cargo.division']
+    @sortable_columns ||= ['Persona.nro_documento', 'Persona.cuil', 'Persona.apeynom','Cargo.secuencia', 'Establecimiento.nombre','Cargo.turno', 'Cargo.anio','Cargo.division', 'Cargo.grupo_id']
   end
 
   def searchable_columns
-    @searchable_columns ||= ['Persona.nro_documento', 'Persona.cuil', 'Persona.apeynom','Cargo.secuencia', 'Establecimiento.nombre', 'Cargo.anio','Cargo.division']
+    @searchable_columns ||= ['Persona.nro_documento', 'Persona.cuil', 'Persona.apeynom','Cargo.secuencia', 'Establecimiento.nombre','Cargo.turno', 'Cargo.anio','Cargo.division']
   end
 
   private
@@ -20,8 +20,10 @@ class CargosBajasDatatable < AjaxDatatablesRails::Base
         record.secuencia,
         record.cargo,
         record.establecimiento.nombre,
+        record.turno,
         record.anio,
         record.division,
+        record.grupo_id,
         '<span class="label label-info">'+Util.fecha_a_es(record.fecha_alta)+'</span>',
         '<a class="btn btn-sm btn-danger popup-fecha" id="'+record.id.to_s+'" data-value="'+Util.fecha_a_es(Date.today)+'" data-type="combodate" data-format="DD/MM/YYYY" data-viewformat="DD-MM-YYYY" data-template="D - MMMM - YYYY" data-container="body" data-placement="left" data-original-title="Seleccione la fecha de baja" data-resource="cargo" data-name="fecha_baja" data-url="'+Rails.application.routes.url_helpers.cargo_dar_baja_path(record.id.to_s)+'">
           <span class="glyphicon glyphicon-trash"></span>
