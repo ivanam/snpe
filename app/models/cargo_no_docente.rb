7542fbfeb24d713492ac2e4c20ec552c24e16a74
Pregase Server <mde@chubut.edu.ar>
2017-07-10 13:42:55 -0300

    cambios migracion

--- app/models/cargo_no_docente.rb
+++ app/models/cargo_no_docente.rb
@@ -6,14 +6,14 @@ class CargoNoDocente < ActiveRecord::Base
   belongs_to :baja_lote_impresion
   has_many :estados, :class_name => 'CargoNoDocenteEstado', :foreign_key => 'cargo_no_docente_id', dependent: :destroy
 
-  # validates :turno, presence: true
-  # validates :fecha_alta, presence: true
-  # validates :cargo, presence: true
-  # validates :situacion_revista, :presence => true
+   validates :turno, presence: true
+   validates :fecha_alta, presence: true
+   validates :cargo, presence: true
+   validates :situacion_revista, :presence => true
   
-  # validate :cargo_existente, if: :no_es_licencia_para_baja
+   validate :cargo_existente, if: :no_es_licencia_para_baja
 
-  # before_update :dar_baja
+   before_update :dar_baja
 
   def no_es_licencia_para_baja
     self.estado != "LIC P/BAJ"
