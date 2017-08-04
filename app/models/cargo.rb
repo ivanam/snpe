8ab0e2182d50639b540ea633372c9b9a477d3e17
Pregase Server <mde@chubut.edu.ar>
2017-08-04 11:19:59 -0300

    asdfas

--- app/models/cargo.rb
+++ app/models/cargo.rb
@@ -8,16 +8,16 @@ class Cargo < ActiveRecord::Base
   has_many :estados, :class_name => 'CargoEstado', :foreign_key => 'cargo_id', dependent: :destroy
 
 
-    validates :fecha_alta, presence: true
-    validates :cargo, presence: true
-    validates :situacion_revista, presence: true
+#    validates :fecha_alta, presence: true
+#    validates :cargo, presence: true
+#    validates :situacion_revista, presence: true
 
 
-    validate :sit_revista
-    validate :cargo_jerarquico
-    validate :controlar_turno
+#    validate :sit_revista
+#    validate :cargo_jerarquico
+#    validate :controlar_turno
 
-   before_update :dar_baja
+#   before_update :dar_baja
 
   #-----------------------------------------------------------------------------------------------------------
 
