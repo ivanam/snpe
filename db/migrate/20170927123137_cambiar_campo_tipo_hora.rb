eb37b43677c39c281a4bb44eb7f7b1a5df8d563f
Pregase Server <mde@chubut.edu.ar>
2017-10-03 11:47:31 -0300

    migracion control

--- db/migrate/20170927123137_cambiar_campo_tipo_hora.rb
+++
@@ -1,6 +0,0 @@
-class CambiarCampoTipoHora < ActiveRecord::Migration
-  def change
-  	  remove_column :altas_bajas_horas, :TipoHora, :string
-      add_column :altas_bajas_horas, :tipo_hora, :string
-  end
-end
