c0386b21c33e29b1bff27015473c213ac8a9deab
Gastón Díaz <gipdiaz@gmail.com>
2017-10-11 12:12:27 -0300

    se agregaron todas las escuelas a los usuarios de personal

--- app/helpers/establecimientos_helper.rb
+++ app/helpers/establecimientos_helper.rb
@@ -2,16 +2,11 @@ module EstablecimientosHelper
   def establecimientos_permitidos
     if current_user.role? :sadmin then
       return Establecimiento.all
+    elsif (current_user.email == "meneira@personal.com") || (current_user.email == "apaz@personal.com") || (current_user.email == "ayalah@personal.com") || (current_user.email == "betancur@personal.com") || (current_user.email == "camposp@personal.com") || (current_user.email == "mcasin@personal.com") || (current_user.email == "mbarrera@personal.com") || (current_user.email == "iribarrenj@personal.com") || (current_user.email == "moralesm@personal.com") || (current_user.email == "njones@personal.com")  || (current_user.email == "sepulvedav@personal.com") 
+      return Establecimiento.where(id: [1,4,8,20,41,47,84,167,185,190,202,401,404,414,441,504,506,508,509,512,523,525,552,603,702,712,725,728,729,732,752,767,774,776,795,7705,7727,3000,3031,4002,4006,28,78,178,403,410,412,453,459,556,2404,2412,4001,4007,5009,567,4000,4004,4009,4012,4014,4015,4016,4017,4018])
     else
-      @establecimiento_ids = []
       current_user.establecimientos.each do |e|
-        if e.peso == 0
-          @establecimiento_ids << e.id
-        else
-          e.suborganizaciones_peso_cero.each do |sub0|
-            @establecimiento_ids << sub0.id
-          end
-        end
+        @establecimiento_ids << e.id
       end
       return Establecimiento.where(id: @establecimiento_ids)
     end
