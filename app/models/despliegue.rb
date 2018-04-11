99cfbd3f51df022da5b09cf887effe117de3bc41
Gastón Díaz <gipdiaz@gmail.com>
2018-04-11 09:51:05 -0300

    correccion en carga de despliegue

--- app/models/despliegue.rb
+++ app/models/despliegue.rb
@@ -3,8 +3,8 @@ class Despliegue < ActiveRecord::Base
   belongs_to :materium
 
 
+  validates :cant_docentes, length: { minimum: 1, maximum: 2}, numericality: { :greater_than => 0, :message => "Ingrese un número mayor a 0" }
   validates :carga_horaria, length: { minimum: 1, maximum: 2}, numericality: { :greater_than => 0, :message => "Ingrese un número mayor a 0" }
-  validates :carga_horaria, length: { minimum: 1, maximum: 2}, numericality: { :greater_than => 0, :message => "Ingrese un número mayor a 0" }
-  validates :fecha_alta, :presence => true
+  validates :anio, :presence => true
 end
 
