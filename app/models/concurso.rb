603880cba26f9bf996fa55c6bc164d4ef30df07e
nicoc <calfuquir.nico@gmail.com>
2018-09-27 08:55:47 -0300

    #25 validando fechas

--- app/models/concurso.rb
+++ app/models/concurso.rb
@@ -1,10 +1,22 @@
 class Concurso < ActiveRecord::Base
-
+  	validate :un_solo_concurso_activo,
+	
 	def es_activo
 		current = DateTime.current
 		result = (self.fecha_inicio < current) and (self.fecha_fin >= current)
 		return result
 	end
 
+	def un_solo_concurso_activo
+		existe = false
+		ini = self.fecha_inicio
+		fin = self.fecha_fin
+		Concurso.all.each do |concurso|
+			if (self.fecha_inicio <= concurso.fecha_fin && self.fecha_fin >= concurso.fecha_inicio)
+				errors.add(:base, "Interposicion de fechas")
+				break
+			end 
+		end 
+  	end
 
 end
