4fa63ab1bbf50245ee47e9b91f2dff22e4fee9d4
Ivana Moyano <iva.moyano@gmail.com>
2018-10-03 13:51:52 -0300

    vista para ver certificado medico

--- config/routes.rb
+++ config/routes.rb
@@ -376,6 +376,8 @@ ProyectoBase::Application.routes.draw do
     get "licencias/autocomplete_persona_apeynom", to: 'licencia#get_autocomplete_items', as: :autocomplete_persona_apeynom_licencia
 
     get "licencias/sin_goce", to: 'licencia#sin_goce', as: :licencias_sin_goce
+    get "licencias/sin_certificado", to: 'licencia#sin_certificado', as: :licencias_sin_certificado
+
     get "util/buscar_hora/:dni", to: 'util#buscar_hora'
     post "util/buscar_hora/:dni", to: 'util#buscar_hora'
     get 'licencia/altas_bajas_horas_licencia_permitida/:dni', to: 'licencia#altas_bajas_horas_licencia_permitida', as: :altas_bajas_horas_licencia_permitida
