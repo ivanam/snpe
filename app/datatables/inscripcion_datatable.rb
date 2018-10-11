3ff07dbb9e2fa4362920b0b3fe63fe318c5e24e0
nicoc <calfuquir.nico@gmail.com>
2018-10-11 10:02:21 -0300

    #31 datatable en instacipcions, filtros y exel.

--- app/datatables/inscripcion_datatable.rb
+++ app/datatables/inscripcion_datatable.rb
@@ -1,12 +1,14 @@
 class InscripcionDatatable < AjaxDatatablesRails::Base
-  include AjaxDatatablesRails::Extensions::WillPaginate
+  include AjaxDatatablesRails::Extensions::Kaminari
 
+  def_delegators :@view, :link_to, :edit_inscripcion_path, :edit_persona_path
+  
   def sortable_columns
-     @sortable_columns ||= ['fecha_incripcion']
+     @sortable_columns ||= ['Inscripcion.fecha_incripcion', 'Region.nombre', 'Inscripcion.cabecera', 'Persona.apeynom']
    end
 
    def searchable_columns
-     @searchable_columns ||= []
+     @searchable_columns ||= ['Inscripcion.fecha_incripcion', 'Region.nombre', 'Inscripcion.cabecera', 'Persona.apeynom', 'Persona.nro_documento']
    end
 
   private
@@ -17,16 +19,16 @@ class InscripcionDatatable < AjaxDatatablesRails::Base
         record.fecha_incripcion,
         record.region.to_s,
         record.cabecera,
-        record.persona.to_s,        
+        link_to(record.persona.to_s, edit_persona_path(record.persona)),
         '<div class="dropdown">
           <button class="btn btn-primary dropdown-toggle" type="button" id="dropdownMenu1" data-toggle="dropdown">
             Acciones
             <span class="caret"></span>
          </button>
            <ul class="dropdown-menu" role="menu" aria-labelledby="dropdownMenu1">
-             <li role="presentation"><a role="menuitem" tabindex="-1" href="inscripcions/'+record.id.to_s+'">Ver</a></li>
-            <li role="presentation"><a role="menuitem" tabindex="-1" href="inscripcions/'+record.id.to_s+'/edit">Editar</a></li>
-            <li role="presentation"><a rel="nofollow" data-method="delete" data-confirm="Seguro desea eliminar" role="menuitem" tabindex="-1" href="inscripcions/'+record.id.to_s+'">Eliminar</a></li>
+             <li role="presentation">'+ link_to("Ver", record)+'</li>
+             <li role="presentation">'+ link_to("Editar", edit_inscripcion_path(record)) +'</li>
+            <li role="presentation">'+ link_to('Eliminar', record, method: :delete, data: { confirm: 'Seguro que desea eliminar?' }) +'</li>
           </ul>
         </div>',
        ]
@@ -34,8 +36,9 @@ class InscripcionDatatable < AjaxDatatablesRails::Base
   end
 
   def get_raw_records
+    Inscripcion.joins(:persona, :region)
     #return options[:query]
-    Inscripcion.all
+    #Inscripcion.all
   end
 
 end
