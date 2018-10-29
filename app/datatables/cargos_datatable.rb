ba0b30fa481bfbac606eeb2f445f537da7b45154
Gonzalo <gjpritzker@gmail.com>
2018-10-29 12:47:01 -0300

    cambios en la pof y cambios en los datables de modificaciones

--- app/datatables/cargos_datatable.rb
+++ app/datatables/cargos_datatable.rb
@@ -30,6 +30,9 @@ class CargosDatatable < AjaxDatatablesRails::Base
         Util.fecha_a_es(record.fecha_baja),
         '<a class="btn btn-primary btn-sm" data-toggle="tooltip" data-placement="top" title="Ver" href="/soft/snpe/cargos/'+record.id.to_s+'"><strong>Ver</strong></a>',
         '<a class="btn btn-success btn-sm" data-toggle="tooltip" data-placement="top" onclicK=editar('+record.id.to_s+') title="Editar"><strong>Editar</strong></a>',
+        if record.estado.to_s == "LIC" then
+        '<a class="btn btn-danger btn-sm" data-toggle="tooltip" data-placement="top" onclicK=licbaj('+record.id.to_s+') title="Lic/Baja"><strong>Lic/Baja</strong></a>'
+        end
 
       ]
     end
