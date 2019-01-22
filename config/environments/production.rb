ee31675347f7d2e8eb05c36345bd569adddbd7f9
Ivana Moyano <iva.moyano@gmail.com>
2019-01-22 13:47:58 -0300

    correo

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -51,8 +51,8 @@ ProyectoBase::Application.configure do
   config.action_mailer.default_url_options = { :host => 'www.chubut.edu.ar/' }
   config.action_mailer.delivery_method = :smtp
   config.action_mailer.smtp_settings = {
-  :address              => "smtp.gmail.com",
-  :port                 => 587,
+  :address              => "smtp.googlemail.com",
+  :port                 => 465,
   :domain               => 'gmail.com',
   :user_name            => 'mecinscripciones20192@gmail.com',
   :password             => '_12345678',
