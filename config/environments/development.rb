2def38f1241283bb3d8c8e07ee9fca0b9a96f822
Pregase Server <mde@chubut.edu.ar>
2019-01-23 11:29:06 -0300

    Cambios en emails para enviar confirmacion

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -42,7 +42,7 @@ ProyectoBase::Application.configure do
   config.action_mailer.smtp_settings = {
   :address              => "smtp.gmail.com",
   :port                 => 587,
-  :user_name            => 'mecinscripciones2018',
+  :user_name            => 'mecinscripciones2019',
   :password             => '_12345678',
   :authentication       => 'plain',
   :enable_starttls_auto => true  }
