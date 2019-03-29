b0d48513c03897b665443d5f2fb5f804164d8d66
Ivana Moyano <iva.moyano@gmail.com>
2019-03-29 13:31:46 -0300

    cmabios en la migracion agrego empresas

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20190313131120) do
+ActiveRecord::Schema.define(version: 20190325122221) do
 
   create_table "active_admin_comments", force: true do |t|
     t.string   "namespace"
@@ -368,6 +368,7 @@ ActiveRecord::Schema.define(version: 20190313131120) do
     t.datetime "updated_at"
     t.integer  "lugar_pago"
     t.boolean  "sede"
+    t.boolean  "migrada"
   end
 
   create_table "establecimientos_users", force: true do |t|
