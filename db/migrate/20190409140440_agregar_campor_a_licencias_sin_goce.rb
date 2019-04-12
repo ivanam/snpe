b7049999cb1706620801c133969a9f8fb18c893e
Ivana Moyano <iva.moyano@gmail.com>
2019-04-12 13:40:49 -0300

    fgg

--- db/migrate/20190409140440_agregar_campor_a_licencias_sin_goce.rb
+++
@@ -1,7 +0,0 @@
-class AgregarCamporALicenciasSinGoce < ActiveRecord::Migration
-  def change
-    add_column :licencia, :cancelada_sin_goce, :boolean
-    add_column :licencia, :obs_sin_goce_cancelacion, :text
-
-  end
-end
