e399f1457182ff22af887775936a80cba4491756
Gonzalo <gjpritzker@gmail.com>
2019-05-02 09:15:37 -0300

    agrego funcion a primaria

--- app/helpers/funcions_helper.rb
+++ app/helpers/funcions_helper.rb
@@ -4,7 +4,7 @@ module FuncionsHelper
     if Establecimiento.find(session[:establecimiento].to_i).nivel_id.to_i == 1 #inicial
       return Funcion.where(categoria: [802, 803, 111, 511, 809, 811, 112, 117, 312, 212, 812, 319, 817, 818, 819, 808] )
     elsif Establecimiento.find(session[:establecimiento].to_i).nivel_id.to_i == 2 # Primaria
-      return Funcion.where(categoria: [305, 306 , 307, 310, 206, 207, 703, 210 ,321 ,322 ,315 ,313 ,113 ,311 ,115 ,111 ,511 ,809 ,611 ,711 ,709 ,811 ,320 ,112 ,117 ,312 ,212 ,617 ,619 ,217 ,219 ,317 ,318 ,319 ,118 ,119 ,817 ,116 ,965 ,108 ,208 ,104 ,808 ,308 ,708] )
+      return Funcion.where(categoria: [305, 306 , 307, 310, 206, 207, 703, 210 ,321 ,322 ,315 ,313 ,113 ,311 ,115 ,111 ,511 ,809 ,611 ,711 ,709 ,811 ,320 ,112 ,117 ,312 ,212 ,617 ,619 ,217 ,219 ,317 ,318 ,319 ,118 ,119 ,817 ,116 ,965 ,108 ,208 ,104 ,808 ,308 ,708, 717] )
     elsif Establecimiento.find(session[:establecimiento].to_i).nivel_id.to_i == 3 # Secundaria
       return Funcion.where(categoria: [943, 913, 914, 915, 315, 313, 311, 115, 111, 935, 312, 963, 964, 965, 944, 960, 961, 962, 928, 929, 930, 916, 917, 918, 306, 925, 926, 927, 919, 920, 928, 929, 930] )
     elsif Establecimiento.find(session[:establecimiento].to_i).nivel_id.to_i == 7 # Especial
