f1114a612a0bfa52ee56081a704fe7984149b164
Ivana Moyano <iva.moyano@gmail.com>
2019-08-07 11:57:05 -0300

    corrijo mensaje estado horas cuando usuario es nil

--- app/models/altas_bajas_hora_estado.rb
+++ app/models/altas_bajas_hora_estado.rb
@@ -15,10 +15,11 @@ class AltasBajasHoraEstado < ActiveRecord::Base
   end
 
    def mensaje_estado
+    
     if User.where(id: self.user_id).first != nil
       email = User.find(self.user_id).email
     else 
-      ""
+      email = " "
     end
      @estado = Estado.where(:id => self.estado_id).first.descripcion.to_s
      if @estado == "Cancelado" then
